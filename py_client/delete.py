0804e3bc87024af1aaec0af0e0347299eed82b4c
apella1 <johncollons5@gmail.com>
2023-09-21 17:04:19 +0300

    Handle delete exception

--- py_client/delete.py
+++ py_client/delete.py
@@ -1,15 +1,24 @@
 """Emulating endpoint consumption"""
 import requests
 
-product_id = input("Which id do you want to use?\n")
+PRODUCT_ID = input("Which ID do you want to use?\n")
 
 try:
-    product_id = int(product_id)
-except:
-    product_id = None
-    print(f"{product_id} is not valid")
+    PRODUCT_ID = int(PRODUCT_ID)
+except ValueError:
+    PRODUCT_ID = None
+    print(f"{PRODUCT_ID} is not a valid integer ID")
 
-if product_id:
-    ENDPOINT = f"http://127.0.0.1:8000/api/products/{product_id}/delete/"
-    get_response = requests.delete(ENDPOINT, timeout=2000)
-    print(get_response.status_code, get_response.status_code == 204)
+if PRODUCT_ID is not None:
+    ENDPOINT = f"http://127.0.0.1:8000/api/products/{PRODUCT_ID}/delete/"
+    try:
+        get_response = requests.delete(ENDPOINT, timeout=5)
+        if get_response.status_code == 204:
+            print(f"Product with ID {PRODUCT_ID} has been deleted succesfully")
+        else:
+            print(f"Failed to delete prouct with ID {PRODUCT_ID}.")
+            print(f"Status code: {get_response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while making the request: {e}")
+else:
+    print("No valid ID provided. Cannot proceed with your request")
